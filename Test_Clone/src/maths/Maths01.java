b45c5a1c0ccb4f882b85a177384dda2c9e660575
apsar <apsar@Apsara-Gurung>
2023-04-11 18:15:45 -0400

    hello

---
+++ Test_Clone/src/maths/Maths01.java
@@ -0,0 +1,18 @@
+package maths;
+
+public class Maths01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a;
+		int b;
+		a=2;
+		b=3;
+		int x;
+		x = (a + b);
+		System.out.println("The sum is equal to "+ x);
+	}
+
+}
